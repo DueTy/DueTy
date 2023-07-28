01cc6926bc066fd77b9e538a08173f7874e77607
DueTy <420770824@qq.com>
2023-07-28 22:30:07 +0800

    修改首页

--- src/views/home-index/index.vue
+++ src/views/home-index/index.vue
@@ -1,340 +1,47 @@
 <template>
-  <div>
-    <div style="">
-      <!--[if mso | IE]><table align="center" border="0" cellpadding="0" cellspacing="0" class="" role="presentation" style="width:600px;" width="600" bgcolor="#f0f0f0" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]-->
-      <div
-        style="
-          background: #f0f0f0;
-          background-color: #f0f0f0;
-          margin: 0px auto;
-          max-width: 600px;
-        "
-      >
-        <table
-          align="center"
-          border="0"
-          cellpadding="0"
-          cellspacing="0"
-          role="presentation"
-          style="background: #f0f0f0; background-color: #f0f0f0; width: 100%"
-        >
-          <tbody>
-            <tr>
-              <td
-                style="
-                  direction: ltr;
-                  font-size: 0px;
-                  padding: 20px 0;
-                  text-align: center;
-                "
-              >
-                <!--[if mso | IE
-                  ]><table
-                    role="presentation"
-                    border="0"
-                    cellpadding="0"
-                    cellspacing="0"
-                  >
-                    <tr></tr>
-                  </table><!
-                [endif]-->
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      <!--[if mso | IE]></td></tr></table><table align="center" border="0" cellpadding="0" cellspacing="0" class="" role="presentation" style="width:600px;" width="600" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><v:rect style="width:600px;" xmlns:v="urn:schemas-microsoft-com:vml" fill="true" stroke="false"><v:fill origin="0, -0.5" position="0, -0.5" src="//1.bp.blogspot.com/-TPrfhxbYpDY/Uh3Refzk02I/AAAAAAAALw8/5sUJ0UUGYuw/s1600/New+York+in+The+1960's+-+70's+(2).jpg" type="frame" size="1,1" aspect="atleast" /><v:textbox style="mso-fit-shape-to-text:true" inset="0,0,0,0"><![endif]-->
-      <div
-        style="
-          background-position: center top;
-          background-repeat: no-repeat;
-          background-size: cover;
-          margin: 0px auto;
-          max-width: 600px;
-        "
-      >
-        <div style="line-height: 0; font-size: 0">
-          <table
-            align="center"
-            border="0"
-            cellpadding="0"
-            cellspacing="0"
-            role="presentation"
-            class="top-table-container"
-          >
-            <tbody>
-              <tr>
-                <td
-                  style="
-                    direction: ltr;
-                    font-size: 0px;
-                    padding: 20px 0;
-                    text-align: center;
-                  "
-                >
-                  <!--[if mso | IE]><table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td class="" style="vertical-align:top;width:600px;" ><![endif]-->
-                  <div
-                    class="mj-column-per-100 mj-outlook-group-fix"
-                    style="
-                      font-size: 0px;
-                      text-align: left;
-                      direction: ltr;
-                      display: inline-block;
-                      vertical-align: top;
-                      width: 100%;
-                    "
-                  >
-                    <table
-                      border="0"
-                      cellpadding="0"
-                      cellspacing="0"
-                      role="presentation"
-                      style="vertical-align: top"
-                      width="100%"
-                    >
-                      <tbody>
-                        <tr>
-                          <td
-                            align="center"
-                            style="
-                              font-size: 0px;
-                              padding: 10px 25px;
-                              word-break: break-word;
-                            "
-                          ></td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </div>
-                  <!--[if mso | IE]></td></tr></table><![endif]-->
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
-      <!--[if mso | IE]></v:textbox></v:rect></td></tr></table><table align="center" border="0" cellpadding="0" cellspacing="0" class="" role="presentation" style="width:600px;" width="600" bgcolor="#fafafa" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]-->
-      <div
-        style="
-          background: #fafafa;
-          background-color: #fafafa;
-          margin: 0px auto;
-          max-width: 600px;
-        "
-      >
-        <table
-          align="center"
-          border="0"
-          cellpadding="0"
-          cellspacing="0"
-          role="presentation"
-          style="background: #fafafa; background-color: #fafafa; width: 100%"
-        >
-          <tbody>
-            <tr>
-              <td
-                style="
-                  direction: ltr;
-                  font-size: 0px;
-                  padding: 20px 0;
-                  text-align: center;
-                "
-              >
-                <!--[if mso | IE]><table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td class="" style="vertical-align:top;width:400px;" ><![endif]-->
-                <div
-                  class="mj-column-px-400 mj-outlook-group-fix"
-                  style="
-                    font-size: 0px;
-                    text-align: left;
-                    direction: ltr;
-                    display: inline-block;
-                    vertical-align: top;
-                    width: 100%;
-                  "
-                >
-                  <table
-                    border="0"
-                    cellpadding="0"
-                    cellspacing="0"
-                    role="presentation"
-                    style="vertical-align: top"
-                    width="100%"
-                  >
-                    <tbody>
-                      <tr>
-                        <td
-                          align="left"
-                          style="
-                            font-size: 0px;
-                            padding: 10px 25px;
-                            word-break: break-word;
-                          "
-                        >
-                          <div
-                            style="
-                              font-family: Helvetica Neue;
-                              font-size: 20px;
-                              font-style: italic;
-                              line-height: 1;
-                              text-align: left;
-                              color: #626262;
-                            "
-                          >
-                            DueTy's
-                          </div>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td
-                          align="left"
-                          style="
-                            font-size: 0px;
-                            padding: 10px 25px;
-                            word-break: break-word;
-                          "
-                        >
-                          <div
-                            style="
-                              font-family: Ubuntu, Helvetica, Arial, sans-serif;
-                              font-size: 13px;
-                              line-height: 1;
-                              text-align: left;
-                              color: #525252;
-                            "
-                          >
-                            程序员、机车男、One Piece、SD...
-                          </div>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-                <!--[if mso | IE]></td></tr></table><![endif]-->
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      <!--[if mso | IE]></td></tr></table><table align="center" border="0" cellpadding="0" cellspacing="0" class="" role="presentation" style="width:600px;" width="600" bgcolor="white" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]-->
-      <div
-        style="
-          background: white;
-          background-color: white;
-          margin: 0px auto;
-          max-width: 600px;
-        "
-      >
-        <table
-          align="center"
-          border="0"
-          cellpadding="0"
-          cellspacing="0"
-          role="presentation"
-          style="background: white; background-color: white; width: 100%"
-        >
-          <tbody>
-            <tr>
-              <td
-                style="
-                  direction: ltr;
-                  font-size: 0px;
-                  padding: 20px 0;
-                  text-align: center;
-                "
-              >
-                <!--[if mso | IE]><table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td class="" style="vertical-align:top;width:600px;" ><![endif]-->
-                <div
-                  class="mj-column-per-100 mj-outlook-group-fix"
-                  style="
-                    font-size: 0px;
-                    text-align: left;
-                    direction: ltr;
-                    display: inline-block;
-                    vertical-align: top;
-                    width: 100%;
-                  "
-                >
-                  <table
-                    border="0"
-                    cellpadding="0"
-                    cellspacing="0"
-                    role="presentation"
-                    style="vertical-align: top"
-                    width="100%"
-                  >
-                    <tbody>
-                      <tr>
-                        <td
-                          align="left"
-                          style="
-                            font-size: 0px;
-                            padding: 10px 25px;
-                            word-break: break-word;
-                          "
-                        >
-                          <div
-                            style="
-                              font-family: Helvetica Neue;
-                              font-size: 20px;
-                              font-style: italic;
-                              line-height: 1;
-                              text-align: left;
-                              color: #626262;
-                            "
-                          >
-                            Find amazing places
-                          </div>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td
-                          align="left"
-                          style="
-                            font-size: 0px;
-                            padding: 10px 25px;
-                            word-break: break-word;
-                          "
-                        >
-                          <div
-                            style="
-                              font-family: Ubuntu, Helvetica, Arial, sans-serif;
-                              font-size: 13px;
-                              line-height: 1;
-                              text-align: left;
-                              color: #525252;
-                            "
-                          >
-                            I'm in love with my machine
-                          </div>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-                <!--[if mso | IE]></td></tr></table><![endif]-->
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      <!--[if mso | IE]></td></tr></table><![endif]-->
+  <div class="page">
+    <div class="banner-container" />
+    <div class="character">
+      <i class="name">DueTy's</i>
+      <p>程序员、机车男、One Piece、SD...</p>
     </div>
+    <i style="color: rgb(82, 82, 82)">I'm in love with my machine</i>
+    <Footer />
   </div>
-  <Footer />
 </template>
 
 <script setup lang="ts">
-import Footer from "@/layouts/Footer.vue"
+import Footer from "@/layouts/Footer.vue";
 </script>
 
 <style scoped lang="less">
-.top-table-container {
+.page {
+  width: 80%;
+  margin: auto;
+  position: relative;
+  min-height: 100vh;
+}
+
+.banner-container {
   background: url(~"@{cdnPrefix}/t-cdn/images/banner.jpeg");
   background-position: right 50%;
   background-repeat: no-repeat;
   background-size: 194%;
   width: 100%;
   height: 300px;
+}
+
+.character {
+  font-family: "Helvetica Neue";
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 110px;
+  flex-direction: column;
+
+  .name {
+    font-size: 20px;
+  }
 }
 #outlook a {
   padding: 0;
