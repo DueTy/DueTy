e80f7d09d9e32970f57834ac26f75827b3c761c7
DueTy <emduetybro@gmail.com>
2024-01-05 16:55:49 +0800

    修改首页内容

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -8,9 +8,9 @@ function App() {
         <div className="banner-container" />
         <div className="character">
           <i className="name">DueTy's</i>
-          <p>程序员、机车男、One Piece、SD...</p>
+          <p>程序员、机车男、One Piece、Slam Dunk...</p>
         </div>
-        <i style={{ color: "rgb(82, 82, 82)" }}>I'm in love with my 400</i>
+        <i style={{ color: "rgb(82, 82, 82)" }}>I'm in love with my Ninja400</i>
         <Footer />
       </div>
     </>
