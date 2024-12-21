122b381b08ce08845c49c1e08a2ffe0337fcc9de
DueTy <420770824@qq.com>
2024-12-21 23:24:32 +0800

    update home page content

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,20 @@
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
+  const env = loadEnv(mode, process.cwd())
   return {
     base: './',
     plugins: [react()],
     resolve: {
       alias: {
-        "@": path.resolve(__dirname, "./src"),
+        '@': path.resolve(__dirname, './src'),
       },
+    },
+    server: {
+      port: 8000,
     },
     css: {
       preprocessorOptions: {
@@ -21,5 +24,5 @@ export default defineConfig(({ mode }) => {
         },
       },
     },
-  };
-});
+  }
+})
